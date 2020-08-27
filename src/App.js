49fc8d22e16dbd25ed2471e02ab117db475d0aad
matyaspapp <project.matyas.papp@protonmail.com>
2020-08-27 19:07:55 +0200

    add grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const NUM_ROWS = 30;
+const NUM_COLS = 30;
+
 function App() {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let rowIdx = 0; rowIdx < NUM_ROWS; rowIdx++) {
+      rows.push(Array(NUM_COLS).fill(0));
+    }
+    return rows;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: `repeat(${NUM_COLS}, 20px)`
+      }}
+    >
+      {grid.map((rows, i) => 
+        rows.map((col, j) =>
+          <div
+            key={`${i}-${j}`}
+            style={{
+              width: 18,
+              height: 18,
+              backgroundColor: grid[i][j] ? "magenta" : "black",
+              border: "solid 1px white"
+            }}
+          />)  
+      )}
     </div>
   );
 }
